d32910c9a47251180e1889b1e1217c4dacf85718
emanaymany <emanayman718@gmail.com>
2019-06-15 15:39:39 +0200

    posts adapter error solved and application run by ahmed

--- app/src/main/java/com/example/parenteye/ChatActivity.java
+++ app/src/main/java/com/example/parenteye/ChatActivity.java
@@ -1,9 +1,12 @@
 package com.example.parenteye;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,24 +14,34 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.text.format.DateFormat;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -36,7 +49,7 @@ public class ChatActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseListAdapter<Chat> adapter;
     RelativeLayout activity_main;
-    FloatingActionButton fab;
+    FloatingActionButton fab,selectImage;
     private CreateTime createTime;
     private TextView friendName;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -45,6 +58,17 @@ public class ChatActivity extends AppCompatActivity {
     final long ONE_MEGABYTE = 1024 * 1024;
     private CircleImageView friendProfile;
     private TextView friendstate;
+
+    private static int Gallery_pick = 1;
+    private DatabaseReference rootref;
+    private ProgressDialog loadingbar;
+    private StorageReference massageImageStorageRef;
+    private TextView frindName;
+    private DatabaseReference userDataBaseRefrance;
+    DatabaseReference usersRef = database.getReference("Users");
+    FirebaseStorage storage = FirebaseStorage.getInstance();//added for test thing
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +82,21 @@ public class ChatActivity extends AppCompatActivity {
         fab=(FloatingActionButton) findViewById(R.id.fab);
         Intent FriendIntent=getIntent();
         final String friendId=FriendIntent.getStringExtra(UserFriendsActivity.friendId);
+
+        selectImage=(FloatingActionButton) findViewById(R.id.selectImage);
+        massageImageStorageRef=storage.getInstance().getReference().child("Massage_pic");
+        //mStorageRef=FirebaseStorage.getInstance().getReference().child("Users");
+        rootref=FirebaseDatabase.getInstance().getReference().child("Chat");
+        loadingbar=new ProgressDialog(this);
+
+
+
+
+
+        //showfriendProfilepicAndname();
+
+
+
         fab.setOnClickListener(new View.OnClickListener(){
 
             @Override
@@ -74,7 +113,7 @@ public class ChatActivity extends AppCompatActivity {
                 }
                 else{
 
-            }
+                }
 
             }
         });
@@ -98,7 +137,7 @@ public class ChatActivity extends AppCompatActivity {
         userRef.child(friendId).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-           String friendname=dataSnapshot.getValue(Users.class).getUsername();
+                String friendname=dataSnapshot.getValue(Users.class).getUsername();
                 friendName.setText(friendname);
                 if(dataSnapshot.getValue(Users.class).getProfile_pic_id()!=null){
                     mStorageRef.child(dataSnapshot.getValue(Users.class).getProfile_pic_id()).getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
@@ -144,6 +183,15 @@ public class ChatActivity extends AppCompatActivity {
                 //get references to the views of list_item.xml
                 TextView messageText,messageUser,messagegetime;
 
+                ImageView messageImage;
+                CircleImageView user_profile_image;
+                //     String fromUserId=model.getMessageUser();
+                //String messageType=model.getmessageType();
+                String messageType=model.getMessageType();
+                //  messageType = FirebaseDatabase.getInstance().getReference().child("Chat").child(mAuth.getCurrentUser().getUid()).child(friendId).child("Type"))
+
+                messageImage=(ImageView) v.findViewById(R.id.massageImageView);
+
                 messageText=(TextView) v.findViewById(R.id.message_text);
                 messageUser=(TextView) v.findViewById(R.id.message_user);
                 messagegetime=(TextView) v.findViewById(R.id.message_time);
@@ -156,13 +204,160 @@ public class ChatActivity extends AppCompatActivity {
                 }
 
 
+
+                if( messageType.equals("text")) {
+                    messageImage.setVisibility(v.INVISIBLE);
+                    messageText.setText(model.getMessageText());
+                    messageUser.setText(model.getMessageUser());
+                    // messagegetime.setText(DateFormat.format("dd/MM/yyyy HH:mm:ss",model.getMessageTime()));
+
+                } else
+                {
+
+                    messageText.setVisibility(v.INVISIBLE);
+                    messageText.setPadding(0,0,0,0);
+
+                    String image=model.getMessageText();
+                    Picasso.get().load(image).into(messageImage);
+                }
+
+
+
                 messageText.setText(model.getMessageText());
                 messageUser.setText(model.getMessageUser());
-            //    messagegetime.setText(DateFormat.format("dd/MM/yyyy HH:mm:ss",model.getMessageTime()));
+                //    messagegetime.setText(DateFormat.format("dd/MM/yyyy HH:mm:ss",model.getMessageTime()));
                 messagegetime.setText(createTime.calculateTime());
             }
         };
         listofmessage.setAdapter(adapter);
+        selectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent galleryintent =new Intent();
+                galleryintent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryintent.setType("image/*");
+                startActivityForResult(galleryintent,Gallery_pick);
+            }
+        });
 
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode== Gallery_pick && resultCode == RESULT_OK && data!=null){
+
+            loadingbar.setTitle("Sending Chat Image");
+            loadingbar.setMessage("Please Wait ,While Your Chat Massage Is Sending ...  ");
+            final EditText input=(EditText) findViewById(R.id.input);
+
+            Uri imageUri =data.getData();
+            Intent FriendIntent=getIntent();
+
+            String friendId=FriendIntent.getStringExtra(UserFriendsActivity.friendId);
+            final String userId=mAuth.getCurrentUser().getUid();
+
+         /*   final String massageSenderRef="Massage/"+friendId+"/"+userId;
+            final String massageReciverRef="Massage/"+userId+"/"+friendId;*/
+
+            final String massageReciverRef=friendId+"/"+userId;
+            final String massageSenderRef=userId+"/"+friendId;
+
+            //  FirebaseDatabase.getInstance().getReference().child("Chat").child(mAuth.getCurrentUser().getUid()).child(friendId).push();
+            DatabaseReference user_massage_key=FirebaseDatabase.getInstance().getReference().child("Chat").child(friendId).child(mAuth.getCurrentUser().getUid()).push();
+
+            final String massage_push_id=user_massage_key.getKey();
+            StorageReference filePath=massageImageStorageRef.child(massage_push_id +".jpg");
+            filePath.putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    if(task.isSuccessful()){
+
+
+                        final String downloadUrl=task.getResult().getMetadata().getReference().getDownloadUrl().toString();
+
+                        Map MassageTextBody=new HashMap();
+                        MassageTextBody.put("messageText",downloadUrl);
+                        //  MassageTextBody.put("seen",false);
+                        MassageTextBody.put("messageType","image");
+                        MassageTextBody.put("time", ServerValue.TIMESTAMP);
+                        MassageTextBody.put("messageUser",userId);
+
+                        Map MassageBodyDetails=new HashMap();
+                        MassageBodyDetails.put(massageSenderRef+"/"+massage_push_id,MassageTextBody);
+                        MassageBodyDetails.put(massageReciverRef+"/"+massage_push_id,MassageTextBody);
+
+
+                        rootref.updateChildren(MassageBodyDetails, new DatabaseReference.CompletionListener() {
+                            @Override
+                            public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+
+                                if(databaseError !=null){
+                                    Log.d("Chat_log",databaseError.getMessage().toString());
+                                }
+                                input.setText("");
+                                loadingbar.dismiss();
+
+                            }
+                        });
+
+
+                        Toast.makeText(ChatActivity.this, "Pictue sent succesfully   ", Toast.LENGTH_SHORT).show();
+
+                        loadingbar.dismiss();
+                    }
+                    else {
+                        Toast.makeText(ChatActivity.this, "picture not sent try again  ", Toast.LENGTH_SHORT).show();
+                        loadingbar.dismiss();
+                    }
+
+                }
+            });
+
+
+
+
+        }
+    }
+
+    /*   public  void showfriendProfilepicAndname(){
+        Intent FriendIntent=getIntent();
+        final String friendId=FriendIntent.getStringExtra(UserFriendsActivity.friendId);
+        usersRef.child(friendId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Users user=dataSnapshot.getValue(Users.class);
+
+
+                frindName.setText(user.getUsername());
+                if (user.getProfile_pic_id()!=null) {
+
+                    String profileImageId=user.getProfile_pic_id();
+                    mStorageRef.child(profileImageId).getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                        @Override
+                        public void onSuccess(byte[] bytes) {
+                            final Bitmap bm = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                            profileImage.setImageBitmap(bm);
+
+                        }
+                    });
+                } else if(user.isGender()==true) {
+                    profileImage.setImageResource(R.drawable.profile_boys);
+
+                }
+                else {
+                    profileImage.setImageResource(R.drawable.profile_giles);
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }*/
+
 }
--- app/src/main/java/com/example/parenteye/MainActivity.java
+++ app/src/main/java/com/example/parenteye/MainActivity.java
@@ -345,12 +345,12 @@ search.setOnClickListener(new View.OnClickListener() {
                 notifi.addNotificationsOfLikes(postid1, "zkoZkODkLSYO8aZFhc0iVOILHc42");
 
                 //addNotificationsOfComments(postid1, LikeridAya);
-                notifi.addNotificationsOfComments(postid2, "zkoZkODkLSYO8aZFhc0iVOILHc42");
-                notifi.addNotificationsOfComments(postid1, "zkoZkODkLSYO8aZFhc0iVOILHc42");
+               /// notifi.addNotificationsOfComments(postid2, "zkoZkODkLSYO8aZFhc0iVOILHc42");
+               //// notifi.addNotificationsOfComments(postid1, "zkoZkODkLSYO8aZFhc0iVOILHc42");
 
                 //Add activitylog on likes , comments
                 activityLog.addActivityLogOfLikes(postid2);
-                activityLog.addActivityLogOfComments(postid1);
+               // activityLog.addActivityLogOfComments(postid1);
 
 
 
@@ -378,7 +378,7 @@ search.setOnClickListener(new View.OnClickListener() {
                 notifi.addNotificationsOfFriendRequest(FriendWantToRequest_Id);
 
                 /// son sends a friend request to
-                activityLog.addActivityLogOfSendFriendRequest(FriendWantToRequest_Id);
+               // activityLog.addActivityLogOfSendFriendRequest(FriendWantToRequest_Id);
 
 
                 //son recieve a friend request from
@@ -428,8 +428,7 @@ search.setOnClickListener(new View.OnClickListener() {
             }
         });
 
-        GetMyHomePosts();
-        showProfilepic();
+             showProfilepic();
 
 
 
@@ -495,76 +494,7 @@ search.setOnClickListener(new View.OnClickListener() {
         finish();
     }
 
-    private void GetMyHomePosts(){
-        if(mAuth.getCurrentUser()!=null){
-            final HomePostsAdapter postadapter=new HomePostsAdapter(MainActivity.this,myposts);
-            Post_listview.setAdapter(postadapter);
-           final ArrayList<String> communityIds=new ArrayList<String>();
-            final ArrayList<String> friendsList=new ArrayList<String>();
 
-            myRef3.child(mAuth.getCurrentUser().getUid()).child("userFriends").addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    if(dataSnapshot.getValue(String.class)!=null) {
-                        String myfriends = dataSnapshot.getValue(String.class);
-                        final String[] myFriendsID = myfriends.split(",");
-                        for(String id:myFriendsID){
-                            friendsList.add(id);
-                        } }
-                    memberRef.addValueEventListener(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                            for(DataSnapshot membersnaphot:dataSnapshot.getChildren()){
-                                if(TextUtils.equals(membersnaphot.getValue(Members.class).getUserId(),mAuth.getCurrentUser().getUid())){
-                                    communityIds.add(membersnaphot.getValue(Members.class).getCommunityid());
-                                }
-                            }
-                          /*  if (dataSnapshot.getValue(Members.class)!=null) {
-                                for (DataSnapshot membersnapshot : dataSnapshot.getChildren()) {
-                                    Members member = membersnapshot.getValue(Members.class);
-                                    communityIds.add(member.getCommunityid());
-                                    //System.out.println(member.getCommunityid());
-                                }
-                            }*/
-                            myRef2.addValueEventListener(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                    myposts.clear();
-                                    for (DataSnapshot postsnapshot : dataSnapshot.getChildren()) {
-                                        Posts post = postsnapshot.getValue(Posts.class);
-                                        if (friendsList.contains(post.getUserId())&&TextUtils.equals(post.getPlaceTypeId(),"1")|| communityIds.contains(post.getPlaceId())) {
-                                            myposts.add(post);
-                                        }
-                                    }
-                                    Collections.reverse(myposts);
-                                    postadapter.notifyDataSetChanged();
-                                }
-
-                                @Override
-                                public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                                }
-                            });
-
-
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                        }
-                    });
-
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                }
-
-            });
-        }
-    }
 
 public  void showProfilepic(){
     myRef.child(mAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
